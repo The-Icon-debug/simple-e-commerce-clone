f01d656cc83a2c5c056024ed7f60085d8a2de15a
Bernard Muli <bernardnthiwa@gmail.com>
2023-02-10 22:25:46 +0300

    routing

--- src/App.js
+++ src/App.js
@@ -3,18 +3,18 @@ import Home from './components/Home';
 import Footer from './components/Footer'
 import AddProduct from './components/AddProduct';
 import NotFound from './components/NotFound';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route, HashRouter} from 'react-router-dom'
 
 function App() {
   return (
-    <Router>
+    <HashRouter>
         <Routes>
           <Route path='/' element={<Home />} />
           <Route exact path="/addproduct" element={<AddProduct/>}/>
           <Route path="*" element={<NotFound/>}/>
         </Routes>
         <Footer />
-    </Router>
+    </HashRouter>
     
   );
 }

81ad6ef6f6a288b895966f33edece826c568798d
Bernard Muli <bernardnthiwa@gmail.com>
2023-02-10 21:53:46 +0300

    Updates

---
+++ precache-manifest.b22e508f026b75c300d2206ed71f1ba8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fcbe5721c88daefa083f",
+    "url": "/simple-e-commerce-clone/static/css/main.11fd865e.chunk.css"
+  },
+  {
+    "revision": "fcbe5721c88daefa083f",
+    "url": "/simple-e-commerce-clone/static/js/main.fcbe5721.chunk.js"
+  },
+  {
+    "revision": "e8e3b76c5bf759905946",
+    "url": "/simple-e-commerce-clone/static/js/1.e8e3b76c.chunk.js"
+  },
+  {
+    "revision": "50ced1dbec3fdd6b9f11",
+    "url": "/simple-e-commerce-clone/static/js/2.50ced1db.chunk.js"
+  },
+  {
+    "revision": "5fa1079d27196ba35714",
+    "url": "/simple-e-commerce-clone/static/js/runtime~main.5fa1079d.js"
+  },
+  {
+    "revision": "9c295622e87660ac9b0c060ecd2d2dd8",
+    "url": "/simple-e-commerce-clone/index.html"
+  }
+];
